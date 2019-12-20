2355c5ac3052d5ca86acef9d9abfb64da436a214
sousajo <josesousa@rentalcars.com>
2019-12-20 17:57:26 +0000

    Added category Service and first exception

---
+++ src/main/java/org/cmsspringfive/newscms/domain/exceptions/CategoryNotFoundException.java
@@ -0,0 +1,6 @@
+package org.cmsspringfive.newscms.domain.exceptions;
+
+public class CategoryNotFoundException extends Throwable {
+    public CategoryNotFoundException(String id) {
+    }
+}
--- src/main/java/org/cmsspringfive/newscms/domain/service/CategoryService.java
+++ src/main/java/org/cmsspringfive/newscms/domain/service/CategoryService.java
@@ -1,5 +1,6 @@
 package org.cmsspringfive.newscms.domain.service;
 
+import org.cmsspringfive.newscms.domain.exceptions.CategoryNotFoundException;
 import org.cmsspringfive.newscms.domain.models.Category;
 import org.cmsspringfive.newscms.domain.repository.CategoryRepository;
 import org.cmsspringfive.newscms.domain.vo.CategoryRequest;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -14,29 +16,49 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
 
+    @Transactional
     public Category update(Category category){
         return this.categoryRepository.save(category);
     }
 
-    public Category create(Category category){
+    @Transactional
+    public Category create(CategoryRequest request){
+        Category category =  new Category();
+        category.setName(request.getName());
         return this.categoryRepository.save(category);
     }
 
+    @Transactional
     public void delete(String id){
-        final Category category = this.categoryRepository.findOne(id);
-        this.categoryRepository.delete(category);
+        final Optional<Category> category = this.categoryRepository.findById(id);
+        category.ifPresent(this.categoryRepository::delete);
     }
 
     public List<Category> findAll(){
+
         return this.categoryRepository.findAll();
     }
 
-    public Category findOne(String id){
-        return this.categoryRepository.findOne(id);
+    public List<Category> findByName(String id){
+        return this.categoryRepository.findByName(id);
+    }
+
+    public List<Category> findByNameStartingWith(String name){
+        return this.categoryRepository.findByNameIgnoreCaseStartingWith(name);
+    }
+
+    public Category findOne(String id) {
+        final Optional<Category> category = this.categoryRepository.findById(id);
+        if(category.isPresent()){
+            return category.get();
+        }else{
+            throw new CategoryNotFoundException(id);
+        }
     }
 
 }
